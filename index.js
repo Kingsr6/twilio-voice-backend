eec871b8f58c37481ff11716dad5e67e950c88f4
Kingsr6 <164889185+Kingsr6@users.noreply.github.com>
2026-05-31 12:12:18 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import twilio from "twilio";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 const client = twilio(
   process.env.TWILIO_ACCOUNT_SID,
@@ -44,4 +48,42 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/token", (req, res) => {
+  const identity = req.query.identity || "user";
+
+  const AccessToken = twilio.jwt.AccessToken;
+  const VoiceGrant = AccessToken.VoiceGrant;
+
+  const voiceGrant = new VoiceGrant({
+    outgoingApplicationSid: TWILIO_TWIML_APP_SID,
+    incomingAllow: true,
+  });
+
+  const token = new AccessToken(
+    TWILIO_ACCOUNT_SID,
+    TWILIO_API_KEY,
+    TWILIO_API_SECRET,
+    {
+      identity,
+      ttl: 3600,
+    }
+  );
+
+  token.addGrant(voiceGrant);
+
+  res.json({
+    token: token.toJwt(),
+    identity,
+  });
+});
+
 app.listen(PORT, () => console.log("Server running on port " + PORT));
+const {
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  TWILIO_API_KEY,
+  TWILIO_API_SECRET,
+  TWILIO_TWIML_APP_SID,
+  TWILIO_CALLER_ID
+} = process.env;
